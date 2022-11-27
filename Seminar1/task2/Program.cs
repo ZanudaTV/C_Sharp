c6e14a98be03b7415b0c3bf3884e46f3982fcc0e
Anton Teslya <antongorichnev@yandex.ru>
2022-11-28 02:35:33 +0300

    fix task2

--- Seminar1/task2/Program.cs
+++ Seminar1/task2/Program.cs
@@ -4,6 +4,17 @@
 44 5 78 -> 78
 22 3 9 -> 22 */
 
+Console.Write("Введите колличество элементов массива:\t");
 
-   int array = new [] { 2, 3, 7 }.Max();
-   Console.WriteLine(array);
+int elements = int.Parse(Console.ReadLine());
+
+int[] array = new int[elements];
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"\nВведите элемент массива под индексом {i}:\t");
+    array[i] = int.Parse(Console.ReadLine());
+}
+
+Console.Write("Максимальное число: ");
+Console.Write(array.Max());
