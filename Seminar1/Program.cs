8a4bbc8771c9e8ad8bfe6f85e8f3bddf0fc07f3d
Anton Teslya <antongorichnev@yandex.ru>
2022-11-28 01:32:36 +0300

    task2

---
+++ Seminar1/Program.cs
@@ -0,0 +1,17 @@
+﻿int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+int Max = 0;
+int Min = 0;
+if (num1 > num2)
+{
+    Max = num1;
+    Min = num2;
+}
+else 
+{
+    Max = num2;
+    Min = num1; 
+}
+
+Console.WriteLine(Max);
+Console.WriteLine(Min);
