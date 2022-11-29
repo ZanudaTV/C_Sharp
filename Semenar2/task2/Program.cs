d18cd135e873c7c1f272c25f280baad4d1f8dadb
Anton Teslya <antongorichnev@yandex.ru>
2022-11-30 02:33:45 +0300

    task2Sem2

---
+++ Semenar2/task2/Program.cs
@@ -0,0 +1,23 @@
+﻿/* Напишите программу, которая выводит третью цифру заданного числа или сообщает,
+ что третьей цифры нет. Не использовать string[]
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6 */
+
+int num = int.Parse(Console.ReadLine());
+
+if (num < 100)
+{
+    Console.WriteLine("Нет третьего числа");
+}
+else
+{
+    while (num > 999)
+    {
+        num /= 10;
+    }
+    int resalt = num / 10;
+    int resalt2 = num - resalt * 10;
+    Console.WriteLine(resalt2);
+}
+
