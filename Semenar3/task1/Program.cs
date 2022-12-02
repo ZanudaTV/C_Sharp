c62cd610f8f2b1945825c02b6cf640b2fef31c2d
Anton Teslya <antongorichnev@yandex.ru>
2022-12-03 01:03:34 +0300

    Sem3Task1

---
+++ Semenar3/task1/Program.cs
@@ -0,0 +1,38 @@
+﻿/* Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да */
+
+
+
+int InputInt(){
+        Console.WriteLine("Введите число ");
+        bool uinput = int.TryParse(Console.ReadLine(),  out int num);
+    if (uinput)
+    {
+        return num;    
+    }
+    else
+    {
+        Console.WriteLine("Вы ввели некорректное значение");
+        return -1;    
+    }
+}
+    int num = InputInt();
+    int newnum = 0;
+        for (int i = num; i > 0; i = (i / 10))
+        {
+            int remainder = i % 10;
+            
+            newnum = (newnum * 10) + remainder;
+        }
+
+        if (newnum == num)
+        {
+            Console.Write($"Число {newnum} является полидромом числа {num}");
+        }
+        else
+        {
+            Console.WriteLine($"Число {newnum} не является полидромом числу {num}");
+        }
+        
