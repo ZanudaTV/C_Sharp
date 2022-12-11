c864bae0d32fd81172e447a0b95f81031db0a2fb
Anton Teslya <antongorichnev@yandex.ru>
2022-12-11 21:14:03 +0300

    task2

---
+++ Semenar5/task2/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных индексах.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+
+int size = InputeSize();
+int[] numbers = new int[size];
+FillArrayRandomNumbers(numbers);
+Console.WriteLine("массив: ");
+PrintArray(numbers);
+int sum = sumNumbersArray();
+
+Console.WriteLine($"всего {numbers.Length} чисел,сумма эллементов стоящих на не четных позициях = {sum}");
+
+int InputeSize()
+{
+    Console.WriteLine("Введите размер массива:   ");
+    bool uinput = int.TryParse(Console.ReadLine(), out int size);
+    if (uinput)
+    {
+        return size;
+    }
+    else
+    {
+        Console.WriteLine("Вы ввели некорректное значение");
+        return -1;
+    }
+}
+int sumNumbersArray()
+{
+int sum = 0;
+
+for (int z = 0; z < numbers.Length; z+=2)
+    sum = sum + numbers[z];
+    return sum;
+}
+void FillArrayRandomNumbers(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = new Random().Next(1,10);
+        }
+}
+void PrintArray(int[] numbers)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            Console.Write(numbers[i] + " ");
+        }
+    Console.Write("]");
+    Console.WriteLine();
+}
